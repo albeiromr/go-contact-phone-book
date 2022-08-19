b60a6dac35450a0dd40df39eaa74cfbcbd5428ac
SOLUDOCTUS\albeiro.marin <albeiro.marin@doctus.com.co>
2022-08-19 10:28:10 -0500

    Agrega comentarios

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"go-contact-phone-book/file-factory"
 )
 
-
 func main() {
 
+	//creando base de datos en formato .csv
 	csvFileError := filefactory.UseFileFactory("database", filefactory.CsvFile)
 	if csvFileError != nil {
 		panic(csvFileError)
 	}
 
+	//creando archivo de logs en formato .log
 	logsFileError := filefactory.UseFileFactory("logs", filefactory.LogFile)
 	if logsFileError != nil {
 		panic(logsFileError)
